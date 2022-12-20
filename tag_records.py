adfc2bf63977ecd1fafcb5740a450f1843f2570f
Hannes Roest <hannes.rost@utoronto.ca>
2022-12-20 16:21:15 -0500

    [FIX] allow zero start for fam/individ

--- tag_records.py
+++ tag_records.py
@@ -8,12 +8,16 @@ def tag_ancestors_families():
     
     gl.get_params()
     family_id = int(gl.initial_family)
+
+    # Perform ancester walk, mark all families that are ancestrial
     tree_walk(family_id)
 
+    # For all marked ancestrial families, tag individuals in those families (including children)
     for i in gl.families:
         if gl.families[i].tag == 'Y':
             tag_family(gl.families[i].family_id, 0)
 
+    # Tag all descendants of those ancestrial families until no more individuals can be tagged
     additional = 1    
     while additional > 0:
         additional = tag_additional_families()
@@ -22,36 +26,36 @@ def tag_ancestors_families():
     gl.write_families()
 
 def tree_walk(family_id):
-    if family_id > 0:
+    if family_id > -1:
         gl.families[family_id].tag = 'Y'   
-        if gl.families[family_id].husband_id != 0:
+        if gl.families[family_id].husband_id != -1:
             husband_id = gl.families[family_id].husband_id
             family_where_child = gl.get_family_where_child(husband_id)
-            if family_where_child != 0:
+            if family_where_child != -1:
                 tree_walk(family_where_child)
 
-        if gl.families[family_id].wife_id != 0:
+        if gl.families[family_id].wife_id != -1:
             wife_id = gl.families[family_id].wife_id
             family_where_child = gl.get_family_where_child(wife_id)
-            if family_where_child != 0:
+            if family_where_child != -1:
                 tree_walk(family_where_child)
 
 def tag_family(family_id, tag_count):
     husband_id = gl.families[family_id].husband_id
-    if husband_id != 0:
+    if husband_id != -1:
         if gl.individuals[husband_id].tag == "N":
             gl.individuals[husband_id].tag = 'Y'
             tag_count = tag_count + 1
             
     wife_id = gl.families[family_id].wife_id
-    if wife_id != 0:
+    if wife_id != -1:
         if gl.individuals[wife_id].tag == "N":
             gl.individuals[wife_id].tag = 'Y'
             tag_count = tag_count + 1
         
     for i in gl.children:
         child_family_id = gl.children[i].family_id
-        if child_family_id != 0:
+        if child_family_id != -1:
             # removes additional children, we dont want that
             # if child_family_id > family_id: break
             if family_id == child_family_id:
